b1fd6de5a3465b5e3479f4a3f6795408847f8d49
Onur Kat <5752017+onurkat@users.noreply.github.com>
2020-09-07 00:57:02 +0300

    Update installjdk.sh

--- installjdk.sh
+++ installjdk.sh
@@ -3,7 +3,7 @@
 # Author : Onur Kat
 # Contact info : github.com/onurkat
 
-echo "This script can install Oracle JDK from the .tar.gz file which provided by Oracle."
+echo "This script can install Oracle JDKs and Open JDKs from the .tar.gz file which provided by Oracle or java.net"
 echo "The script will generate the necessary configuration files for you to use the JDK." 
 echo "If you wish, you can set the newly installed JDK as the JDK to be used after installation."
 echo "Do you want to continue the installation?"
